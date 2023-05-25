34cc1823db2efc89e4edb6a524ba73ccf188afc6
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-05-25 15:29:52 +0200

    Premier partie de la correction

--- src/main/java/fr/eni/movielibrary/ihm/MovieController.java
+++ src/main/java/fr/eni/movielibrary/ihm/MovieController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import fr.eni.movielibrary.bll.MovieService;
 import fr.eni.movielibrary.bo.Movie;
 
-@Component("movieBean")
+@Controller
 public class MovieController {
 
 	@Autowired
@@ -20,5 +24,26 @@ public class MovieController {
 	
 	public List<Movie> showAllMovies(){
 		return movieService.getAllMovies();
+	}
+	@GetMapping("movie/{id}")
+	public String movie(@PathVariable("id") int id, Model model) {
+		
+		// Récupérer une personne mock
+		Movie movie = movieService.getMovieById(id);
+		
+		// Envoyer dans le model
+		model.addAttribute("movie", movie);
+		
+		return "movie/movie-form";
+	}
+
+	
+	@GetMapping("movies")
+	public String showMovies(Model model) {		
+		// 1 : Envoyer les données dans le model pour la vue
+		model.addAttribute("movies", movieService.getAllMovies());
+		
+		// 2 : Retourner la vue
+		return "movie/movie-list";
 	}
 }
